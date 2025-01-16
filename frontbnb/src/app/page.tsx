56bf38e36f0b17edf6b09a4c29e7ec2522870189
Lyrecoph <lyrecoph30@gmail.com>
2025-01-16 18:53:27 +0100

    List properties on the front page static

--- frontbnb/src/app/page.tsx
+++ frontbnb/src/app/page.tsx
@@ -1,4 +1,5 @@
 import Categories from "./components/Categories";
+import PropertyList from "./components/properties/PropertyList";
 
 export default function Home() {
   return (
@@ -6,6 +7,9 @@ export default function Home() {
       <main className="max-w-[1500px] mx-auto px-6">
         Django BNB
         <Categories />
+        <div className="mt-4 grid grid-cols-1 md:grid-cols-3 lg:grid-cols-5 gap-6">
+          <PropertyList />
+        </div>
       </main>
   );
 }
