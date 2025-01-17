273ca28876b9e0da40cef10e38224e99ba803f98
Lyrecoph <lyrecoph30@gmail.com>
2025-01-17 03:29:08 +0100

    user menu popup

--- frontbnb/src/app/components/Navbar/UserNav.tsx
+++ frontbnb/src/app/components/Navbar/UserNav.tsx
@@ -1,6 +1,15 @@
+'use client';
+
+import { useState } from "react";
+import MenuLink from "./MenuLink";
+
 const UserNav = () => {
+    const [isOpen, setIsOpen] = useState(false);
     return(
-        <div className="p-2 relative inline-block border rounded-full">
+        <div 
+            onClick={() => setIsOpen(!isOpen)}
+            className="p-2 relative inline-block border rounded-full"
+        >
             <button className="flex items-center">
             <svg fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-6">
                 <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
@@ -10,6 +19,19 @@ const UserNav = () => {
             </svg>
 
             </button>
+            {isOpen && (
+                <div className="w-[220px] absolute top-[60px] right-0 bg-white border rounded-xl shadow-md flex flex-col cursor-pointer">
+                    <MenuLink 
+                        label="Log in"
+                        onClick={() => console.log('clicked button')}
+                    />
+
+                    <MenuLink 
+                        label="Sign up"
+                        onClick={() => console.log('clicked button')}
+                    />
+                </div>
+            )}
         </div>
         
         
