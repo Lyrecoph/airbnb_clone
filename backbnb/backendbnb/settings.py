3f8097f0e54c51a6227b552db1f4ee7c5803228b
Lyrecoph <lyrecoph30@gmail.com>
2025-01-21 18:37:45 +0100

    create app, model and serializer for properties

--- backbnb/backendbnb/settings.py
+++ backbnb/backendbnb/settings.py
@@ -1,5 +1,6 @@
 import os
 
+from datetime import timedelta
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -9,16 +10,62 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
+# SECRET_KEY depuis l'environnement
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
-# SECURITY WARNING: don't run with debug turned on in production!
+# Debug mode
 DEBUG = bool(os.environ.get("DEBUG", default=0))
 
+# Configuration des hôtes autorisés
 ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS").split(" ")
 
 # Remplace le modèl par défaut (django.contrib.auth.models.User) par le modèle User. 
 AUTH_USER_MODEL = 'useraccount.User'
+
+SITE_ID = int(os.environ.get("SITE_ID", 1))
+
+WEBSITE_URL = os.environ.get("WEBSITE_URL", "http://localhost:8000")
+
+# Configuration JWT
+SIMPLE_JWT = {
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=int(os.environ.get("JWT_ACCESS_TOKEN_LIFETIME", 60))),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=int(os.environ.get("JWT_REFRESH_TOKEN_LIFETIME", 7))),
+    "ROTATE_REFRESH_TOKEN": False,
+    "BLACKLIST_AFTER_ROTATION": False,
+    "UPDATE_LAST_LOGIN": True,
+    "SIGNING_KEY": os.environ.get("SECRET_KEY"),
+    "ALGORITHM": os.environ.get("JWT_ALGORITHM", "HS256"),
+}
+
+# Configuration Django Allauth
+ACCOUNT_USER_MODEL_USERNAME_FIELD = os.environ.get("ACCOUNT_USER_MODEL_USERNAME_FIELD", None),
+ACCOUNT_EMAIL_REQUIRED = True
+ACCOUNT_USERNAME_REQUIRED = False
+ACCOUNT_AUTHENTICATION_METHOD = os.environ.get("ACCOUNT_AUTHENTICATION_METHOD", "email")
+ACCOUNT_EMAIL_VERIFICATION = os.environ.get("ACCOUNT_EMAIL_VERIFICATION", None)
+
+
+# Configuration Django Rest Framework
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ],
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ],
+}
+    
+# CORS Configuration
+CORS_ALLOWED_ORIGINS = os.environ.get("CORS_ALLOWED_ORIGINS", "").split(",")
+
+# CSRF Configuration
+CSRF_TRUSTED_ORIGINS = os.environ.get("CSRF_TRUSTED_ORIGINS", "").split(",")
+
+
+REST_AUTH = {
+    "USE_JWT": True,
+    "JWT_AUTH_HTTPONLY": False
+}
 
 # Application definition
 
@@ -30,12 +77,26 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     
+    'rest_framework',
+    'rest_framework.authtoken',
+    'rest_framework_simplejwt',
+    
+    'allauth',
+    'allauth.account',
+    
+    'dj_rest_auth',
+    'dj_rest_auth.registration',
+    
+    'corsheaders',
+    
     'useraccount.apps.UseraccountConfig',
+    'property.apps.PropertyConfig',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -113,7 +174,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+# Gestion des fichiers statiques et médias
+STATIC_URL = os.environ.get("STATIC_URL", "static/")
+MEDIA_URL = os.environ.get("MEDIA_URL", "media/")
+MEDIA_ROOT = BASE_DIR / 'media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
